0d0b0084837bf97cdb6310edabbd02852e41c212
mateussf99 <mateus.ms65@gmail.com>
2024-03-19 19:59:17 -0300

    :construction: ve o ataque o subterraneo e as que faltam

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,10 +4,13 @@ extends CharacterBody2D
 const SPEED = 200.0
 const JUMP_FORCE = -350.0
 
+
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = 1100
 var is_jumping := false
 
+var can_track_input: bool = true
 @onready var animation:= $anim as AnimatedSprite2D
 
 func _physics_process(delta):
@@ -24,7 +27,7 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("ui_left", "ui_right") 
 	if direction:
 		velocity.x = direction * SPEED
 		animation.scale.x = direction
@@ -35,5 +38,10 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		animation.play("cat")
+	if Input.is_action_just_pressed("attackq") and is_on_floor():
+		animation.play("atackpa")
+	if Input.is_action_just_pressed("attacke") and is_on_floor():
+		animation.play("atackluz")
+	
 
 	move_and_slide()
